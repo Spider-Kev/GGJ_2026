6c22e7eb636bcccec9262b496aacceabfc7474fd
Spider-Kev <74723935+Spider-Kev@users.noreply.github.com>
2026-01-27 17:25:11 -0600

    Plugin habilitado

---
+++ Source/GGJ_2026/Public/CPP_MainPlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperZDCharacter.h"
+#include "CPP_MainPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GGJ_2026_API ACPP_MainPlayer : public APaperZDCharacter
+{
+	GENERATED_BODY()
+	
+};
