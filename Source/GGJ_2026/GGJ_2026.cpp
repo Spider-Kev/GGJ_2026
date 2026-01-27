cf0e4e814fb87a5c6406cd88e081721cfa2f78a7
Spider-Kev <74723935+Spider-Kev@users.noreply.github.com>
2026-01-27 14:53:30 -0600

    Project blank overrida

--- Source/GGJ_2026/GGJ_2026.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GGJ_2026.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GGJ_2026, "GGJ_2026" );
-
-DEFINE_LOG_CATEGORY(LogGGJ_2026)
- 
--- Source/GGJ_2026/GGJ_2026GameMode.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "GGJ_2026GameMode.h"
-
-AGGJ_2026GameMode::AGGJ_2026GameMode()
-{
-	// stub
-}
